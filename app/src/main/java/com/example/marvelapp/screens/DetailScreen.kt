91fbad4cb065fca25727ee599ac9e6bd1b871c6e
Скороженко Елизавета <skoroliza@mail.ru>
2024-04-19 14:23:28 +0600

    обновлен DetailScreen

--- app/src/main/java/com/example/marvelapp/screens/DetailScreen.kt
+++ app/src/main/java/com/example/marvelapp/screens/DetailScreen.kt
@@ -14,24 +14,83 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
-import com.example.marvelapp.screens.components.getHeroById
+import com.example.marvelapp.retrofit.DetailViewModel
 import com.example.marvelapp.ui.theme.AppTheme
 
+//@Composable
+//fun DetailScreen(viewModel: DetailViewModel = viewModel(), onClick: () -> Unit) {
+//    val heroItem by viewModel.heroDataModel.collectAsState()
+//    val hero = heroItem.heroModel?.data?.results?.first()
+//
+//    Box(modifier = Modifier.fillMaxSize()) {
+//        if (hero != null) {
+//            AsyncImage(
+//                model = hero.thumbnail.path + hero.thumbnail.extension,
+//                contentDescription = null,
+//                modifier = Modifier.fillMaxSize(),
+//                contentScale = ContentScale.Crop
+//            )
+//
+//            Button(
+//                onClick = { onClick() },
+//                modifier = Modifier
+//                    .align(Alignment.TopStart)
+//                    .padding(AppTheme.Paddings.HeroDetailButton),
+//                colors = ButtonDefaults.buttonColors(Color.Transparent),
+//                shape = CircleShape
+//            ) {
+//                Icon(
+//                    imageVector = Icons.Default.ArrowBack,
+//                    contentDescription = null,
+//                    tint = AppTheme.ButtonColor.buttonColor
+//                )
+//            }
+//
+//            Box(
+//                modifier = Modifier
+//                    .align(Alignment.BottomStart)
+//                    .padding(AppTheme.Paddings.HeroDetailHorizontalPadding)
+//            ) {
+//                Column(Modifier.padding(AppTheme.Paddings.HeroDetailTextPadding)) {
+//                    Text(
+//                        text = hero.name,
+//                        style = AppTheme.TextStyle.Default_34,
+//                        color = AppTheme.TextColors.white,
+//                    )
+//                    Spacer(modifier = Modifier.height(AppTheme.Paddings.HeroDetailSpacer))
+//                    Text(
+//                        text = hero.description,
+//                        style = AppTheme.TextStyle.Default_24,
+//                        color = AppTheme.TextColors.white,
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+
 @Composable
-fun DetailScreen(heroId: Int, onClick: () -> Unit) {
-    val hero = getHeroById(heroId)
+fun DetailScreen(viewModel: DetailViewModel = viewModel(), onClick: () -> Unit) {
+
+    val heroItem by viewModel.heroDataModel.collectAsState()
+    val hero = heroItem.heroModel?.data?.results?.first()
+
 
     Box(modifier = Modifier.fillMaxSize()) {
         if (hero != null) {
             AsyncImage(
-                model = hero.imageUrl,
+                model = hero.thumbnail.path + hero.thumbnail.extension,
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxSize(),
@@ -60,13 +119,13 @@ fun DetailScreen(heroId: Int, onClick: () -> Unit) {
             ) {
                 Column(Modifier.padding(AppTheme.Paddings.HeroDetailTextPadding)) {
                     Text(
-                        text = stringResource(id = hero.name),
+                        text = hero.name,
                         style = AppTheme.TextStyle.Default_34,
                         color = AppTheme.TextColors.white,
                     )
                     Spacer(modifier = Modifier.height(AppTheme.Paddings.HeroDetailSpacer))
                     Text(
-                        text = stringResource(id = hero.info),
+                        text = hero.description,
                         style = AppTheme.TextStyle.Default_24,
                         color = AppTheme.TextColors.white,
                     )
@@ -75,9 +134,8 @@ fun DetailScreen(heroId: Int, onClick: () -> Unit) {
         }
     }
 }
-
 @Preview
 @Composable
 fun DetailScreenPreview() {
-    DetailScreen(heroId = 3, onClick = {})
+    //DetailScreen(heroId = 3, onClick = {})
 }
