a4e5d2725d1292b05b48b65ded3e0fd7a849b6a2
Скороженко Елизавета <skoroliza@mail.ru>
2024-04-28 16:10:44 +0600

    обновлен MarvelApi

--- app/src/main/java/com/example/marvelapp/network/api/MarvelApi.kt
+++ app/src/main/java/com/example/marvelapp/network/api/MarvelApi.kt
@@ -1,6 +1,6 @@
-package com.example.marvelapp.retrofit.api
+package com.example.marvelapp.network.api
 
-import com.example.marvelapp.screens.components.HeroDataModel
+import com.example.marvelapp.ui.components.HeroDataModel
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -8,14 +8,13 @@ import retrofit2.http.Query
 interface MarvelApi {
     @GET("v1/public/characters")
     suspend fun getHeroesList(
-        @Query("limit") limit: Long = 7,
-        //@Query("apikey") apiKey: String = "24443ee7c47f138c9773b5e6cef4c2c7"
-    ) : HeroDataModel
+        @Query("limit") limit: Long = 7
+    ): HeroDataModel
 
     @GET("v1/public/characters/{characterId}")
     suspend fun getHeroById(
         @Path("characterId") characterId: Int
-    ) : HeroDataModel
+    ): HeroDataModel
 }
 
 
