aa4b4455bda590f4e99178d45c8c59947821caa0
Скороженко Елизавета <skoroliza@mail.ru>
2024-04-28 16:10:57 +0600

    обновлен MainActivity

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -3,61 +3,48 @@ package com.example.marvelapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.core.view.WindowCompat
 import com.example.marvelapp.navigation.Navigation
-import com.example.marvelapp.retrofit.api.MarvelApi
-import com.example.marvelapp.retrofit.repository.MarvelRepository
-import com.example.marvelapp.retrofit.repository.MarvelRepositoryImpl
-import com.example.marvelapp.retrofit.MarvelViewModel
-import com.example.marvelapp.retrofit.RetrofitService
-import dagger.Component
+import com.example.marvelapp.ui.screens.detail.DetailViewModel
+import com.example.marvelapp.data.repository.MarvelRepository
+import com.example.marvelapp.ui.screens.marvel.MarvelViewModel
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.internal.GeneratedComponent
-import javax.inject.Inject
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity(), GeneratedComponent {
+class MainActivity : ComponentActivity() {
 
-//    @Inject
-//    lateinit var marvelRepository: MarvelRepository
-
-    @Inject
-    lateinit var marvelViewModel: MarvelViewModel
+    private val marvelViewModel: MarvelViewModel by viewModels()
+    private val detailViewModel: DetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        val appComponent = DaggerAppComponent.create()
-        appComponent.inject(this)
-        //appComponent.inject(marvelRepository)
-        appComponent.inject(marvelViewModel)
-
         setContent {
-            Navigation()
+            Navigation(marvelViewModel, detailViewModel)
         }
     }
 }
 
-@Component(modules = [AppModule::class, RetrofitService::class])
-interface AppComponent {
-    fun inject(mainActivity: MainActivity)
-    fun inject(viewModel: MarvelViewModel)
-    fun inject(marvelRepository: MarvelRepository)
-    //fun marvelViewModel(): MarvelViewModel
-}
-
 @Module
-class AppModule {
+@InstallIn(ViewModelComponent::class)
+object ViewModelModule {
+
     @Provides
-    fun provideMarvelRepository(api: MarvelApi): MarvelRepository {
-        return MarvelRepositoryImpl(api)
+    @ViewModelScoped
+    fun provideMarvelViewModel(marvelRepository: MarvelRepository): MarvelViewModel {
+        return MarvelViewModel(marvelRepository)
     }
 
     @Provides
-    fun provideMarvelViewModel(repository: MarvelRepository): MarvelViewModel {
-        return MarvelViewModel(repository)
+    @ViewModelScoped
+    fun provideDetailViewModel(marvelRepository: MarvelRepository): DetailViewModel {
+        return DetailViewModel(marvelRepository)
     }
 }
